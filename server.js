8762eee0139c55ac43015779896f40377602cb37
laluisguerra <lguerra@ext.bancointernacional.cl>
2021-01-08 18:46:35 -0300

    modificacion server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const http = require('http');
 const path = require('path');
-let app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || '8080';
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on :${port}`));
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+   console.log('Server is up!');
+});
